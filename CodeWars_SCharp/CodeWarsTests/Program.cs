cf6eefc04bc2f2f97b3b7dccab2b62554506d054
Vladimir Trofimov <trofimov57@mail.ru>
2021-11-24 12:13:53 +0200

    initial commit

---
+++ CodeWars_SCharp/CodeWarsTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeWarsTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
